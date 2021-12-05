5b121aefaeaf1d88d6a3c398051cbe276f27fbbf
imufiid <imufiid98@gmail.com>
2021-12-05 16:38:01 +0700

    Init project, create simple calculator

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -9,11 +9,123 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sumClicked(_ sender: Any) {
+        var firstNumb = 0
+        var secondNumb = 0
+        if let fNumb = Int(firstNumber.text ?? "") {
+            firstNumb = fNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        if let sNumb = Int(secondNumber.text ?? "") {
+            secondNumb = sNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        calculate(firstNumb: firstNumb, secondNumb: secondNumb, action: "+")
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        var firstNumb = 0
+        var secondNumb = 0
+        if let fNumb = Int(firstNumber.text ?? "") {
+            firstNumb = fNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        if let sNumb = Int(secondNumber.text ?? "") {
+            secondNumb = sNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        calculate(firstNumb: firstNumb, secondNumb: secondNumb, action: "-")
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        var firstNumb = 0
+        var secondNumb = 0
+        if let fNumb = Int(firstNumber.text ?? "") {
+            firstNumb = fNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        if let sNumb = Int(secondNumber.text ?? "") {
+            secondNumb = sNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        calculate(firstNumb: firstNumb, secondNumb: secondNumb, action: "*")
+    }
+    
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        var firstNumb = 0
+        var secondNumb = 0
+        if let fNumb = Int(firstNumber.text ?? "") {
+            firstNumb = fNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        if let sNumb = Int(secondNumber.text ?? "") {
+            secondNumb = sNumb
+        } else {
+            showToast(controller: self, message: "Wrong Input", second: 1)
+            return
+        }
+        
+        calculate(firstNumb: firstNumb, secondNumb: secondNumb, action: "/")
+    }
+    
+    private func showToast(controller: UIViewController, message: String, second: Double) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .white
+        alert.view.alpha = 0.5
+        alert.view.layer.cornerRadius = 15
+        
+        controller.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + second) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+    private func calculate(firstNumb: Int, secondNumb: Int, action: String) {
+        var result = 0
+        switch action {
+            case "+":
+                result = firstNumb + secondNumb
+            case "-":
+                result = firstNumb - secondNumb
+            case "*":
+                result = firstNumb * secondNumb
+            case "/":
+                result = firstNumb / secondNumb
+            default:
+                showToast(controller: self, message: "Wrong Input", second: 1)
+        }
+        
+        resultLable.text = "Result: \(result)"
+    }
 }
 
